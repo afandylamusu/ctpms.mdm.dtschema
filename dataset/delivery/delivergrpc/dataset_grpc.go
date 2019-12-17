703b618e0dee588f1b22781de806de8a76b2b164
Sjafrudin Lamusu <afandy.lamusu@yahoo.com>
2019-12-17 12:10:32 +0700

    config RunGRPCServer

--- dataset/delivery/delivergrpc/dataset_grpc.go
+++ dataset/delivery/delivergrpc/dataset_grpc.go
@@ -2,19 +2,14 @@ package delivergrpc
 
 import (
 	"context"
-	"log"
-	"net"
-	"time"
-
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/keepalive"
 )
 
-type Server struct {
-	port string
+// DataSetServiceHandler the GRPC Handler
+type DataSetServiceHandler struct {
+	Port string
 }
 
-func (s *Server) Add(ctx context.Context, request *Request) (*Response, error) {
+func (s *DataSetServiceHandler) Add(ctx context.Context, request *Request) (*Response, error) {
 	a, b := request.GetA(), request.GetB()
 
 	result := a + b
@@ -22,37 +17,11 @@ func (s *Server) Add(ctx context.Context, request *Request) (*Response, error) {
 	return &Response{Result: result}, nil
 }
 
-func (s *Server) Multiply(ctx context.Context, request *Request) (*Response, error) {
+// Multiply of calculator
+func (s *DataSetServiceHandler) Multiply(ctx context.Context, request *Request) (*Response, error) {
 	a, b := request.GetA(), request.GetB()
 
 	result := a * b
 
 	return &Response{Result: result}, nil
-}
-
-func RunServer(port string) {
-	lis, err := net.Listen("tcp", port)
-	if err != nil {
-		log.Fatalf("failed to listen: %v", err)
-	}
-	s := grpc.NewServer(
-		grpc.ConnectionTimeout(time.Second),
-		grpc.KeepaliveParams(keepalive.ServerParameters{
-			MaxConnectionIdle: time.Second * 10,
-			Timeout:           time.Second * 20,
-		}),
-		grpc.KeepaliveEnforcementPolicy(
-			keepalive.EnforcementPolicy{
-				MinTime:             time.Second,
-				PermitWithoutStream: true,
-			}),
-		grpc.MaxConcurrentStreams(5),
-	)
-
-	RegisterAddServiceServer(s, &Server{port})
-	log.Println("Run GRPC AddServiceServer: " + port)
-
-	if err := s.Serve(lis); err != nil {
-		log.Fatalf("failed to serve: %v", err)
-	}
 }
--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"database/sql"
 	"fmt"
 	"log"
+	"net"
 	"net/url"
 	"os"
 	"time"
@@ -11,13 +12,15 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/labstack/echo"
 	"github.com/spf13/viper"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/keepalive"
 
 	_articleHttpDeliver "github.com/afandylamusu/ctpms.mdm.dtschema/article/delivery/http"
 	_articleRepo "github.com/afandylamusu/ctpms.mdm.dtschema/article/repository"
 	_articleUcase "github.com/afandylamusu/ctpms.mdm.dtschema/article/usecase"
 	_authorRepo "github.com/afandylamusu/ctpms.mdm.dtschema/author/repository"
+	_deliverGrpc "github.com/afandylamusu/ctpms.mdm.dtschema/dataset/delivery/delivergrpc"
 	"github.com/afandylamusu/ctpms.mdm.dtschema/middleware"
-	"github.com/afandylamusu/ctpms.mdm.dtschema/dataset/delivery/delivergrpc"
 )
 
 func init() {
@@ -30,6 +33,37 @@ func init() {
 	if viper.GetBool(`debug`) {
 		fmt.Println("Service RUN on DEBUG mode")
 	}
+}
+
+// RunGRPCServer to starting GRPC Server
+func RunGRPCServer(port string) error {
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	s := grpc.NewServer(
+		grpc.ConnectionTimeout(time.Second),
+		grpc.KeepaliveParams(keepalive.ServerParameters{
+			MaxConnectionIdle: time.Second * 10,
+			Timeout:           time.Second * 20,
+		}),
+		grpc.KeepaliveEnforcementPolicy(
+			keepalive.EnforcementPolicy{
+				MinTime:             time.Second,
+				PermitWithoutStream: true,
+			}),
+		grpc.MaxConcurrentStreams(5),
+	)
+
+	_deliverGrpc.RegisterAddServiceServer(s, &_deliverGrpc.DataSetServiceHandler{Port: port})
+	log.Println("Run GRPC AddServiceServer: " + port)
+
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+
+	return err
 }
 
 func main() {
@@ -84,9 +118,9 @@ func main() {
 	au := _articleUcase.NewArticleUsecase(ar, authorRepo, timeoutContext)
 	_articleHttpDeliver.NewArticleHandler(e, au)
 
-	go delivergrpc.RunServer(":9091")
+	go RunGRPCServer(viper.GetString("server.grpc-port"))
 
-	log.Fatal(e.Start(viper.GetString("server.address")))
+	log.Fatal(e.Start(viper.GetString("server.http-port")))
 
 	<-make(chan int)
 }
